937ecfd9fba576c0db2db358a2b300be577a9103
RasheedJohnson <83765797+RasheedJohnson@users.noreply.github.com>
2023-08-14 10:26:47 +0200

    add middleware and .e/route.ts. complete profile/page.tsx getUserDetails button

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,10 +3,12 @@ import axios from "axios";
 import Link from "next/link";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 
 export default function ProfilePage() {
   const router = useRouter()
+  const [data, setData] = useState("nothing");
   const logout = async () => {
     try {
       await axios.get("/api/users/logout")
@@ -16,8 +18,13 @@ export default function ProfilePage() {
     } catch (error: any) {
       console.log(error.message);
       toast.error(error.message);
-
     }
+  }
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me")
+    console.log(res.data);
+    setData(res.data.data._id)
   }
 
   return (
@@ -30,14 +37,25 @@ export default function ProfilePage() {
       <hr className="my-14 opacity-20 w-56" />
       <h1>Profile</h1>
       <hr className="my-14 opacity-20 w-56" />
-      <p>Profile Page</p>
+      <h2 className="p-5 rounded-sm bg-purple-700">
+        {data === "nothing" ? "No Data" : <Link href={`/profile/${data}`}>
+          { data }
+          </Link>}
+      </h2>
 
       <hr className="my-14 opacity-20 w-56" />
       <button
           onClick={logout}
-          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+          className="p-2 border border-gray-300 rounded-sm mb-4 focus:outline-none focus:border-gray-600"
         >
         Logout
+      </button>
+
+      <button
+          onClick={getUserDetails}
+          className="p-2 rounded-sm mb-4 bg-purple-700"
+        >
+        Get User Details
       </button>
     </div>
   );
